6d3c03f5de630326fa2eb6bb6c4844bcc5247cad
Kendall Arneaud <kendall.arneaud@gmail.com>
2022-11-10 16:41:46 -0400

    Create AbstractCity.php
    add base class for handling shipping city

---
+++ src/Common/Object/AbstractCity.php
@@ -0,0 +1,34 @@
+ <?php
+namespace Omniship\Common\Object;
+
+use Omniship\Common\ParametersTrait;
+
+abstract class AbstractCity {
+  
+    use ParametersTrait;
+  
+    function setName($value) {
+        $this->setParameter("name", $value);
+    }
+
+    function setZipCode($value) {
+        $this->setParameter('zip_code', $value);
+    }
+  
+    function setShortName($value) {
+        $this->setParameter('short_name', $value);
+    }
+  
+    function getName() {
+      return $this->getParameter('name');
+    }
+  
+    function getShortName() {
+      return $this->getParameter('short_name');
+    }
+  
+    function getZipCode() {
+        return $this->setParameter('zip_code');
+    }
+  
+}
